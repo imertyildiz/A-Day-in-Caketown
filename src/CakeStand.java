2224c1b1c4516d2f61634d2205e0db5437df0abb
imertyildiz <e2310654@ceng.metu.edu.tr>
2022-05-28 17:51:59 +0300

    Last hopefully

--- src/CakeStand.java
+++ src/CakeStand.java
@@ -7,14 +7,13 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class CakeStand {
-    static Lock supplyLock = new ReentrantLock();
-    static Lock putLock = new ReentrantLock();
+    static Lock lock = new ReentrantLock();
     Lock monsterLock = new ReentrantLock();
-    static Condition supplyCondition = supplyLock.newCondition();
-    static Condition anyStandCondition = supplyLock.newCondition();
-    static Condition putCakeCondition = putLock.newCondition();
+    static Condition supplyCondition = lock.newCondition();
+    static Condition anyStandCondition = lock.newCondition();
+    static Condition putCakeCondition = lock.newCondition();
     Condition standCondition = monsterLock.newCondition();
-    static Condition bakerCondition = putLock.newCondition();
+    static Condition bakerCondition = lock.newCondition();
     static List<CakeStand> synchronizedList = Collections.synchronizedList(new ArrayList<>());
     static List<CakeStand> availableStandList = Collections.synchronizedList(new ArrayList<>());
     static int supplierNumber =0;
@@ -30,7 +29,7 @@ public class CakeStand {
     }
 
     public static void supplyStand(){
-        supplyLock.lock(); // Acquire the lock
+        lock.lock(); // Acquire the lock
         System.out.println(Thread.currentThread().getName() + " brought a new stand.");
         try {
             while (supplierNumber > 0) {
@@ -53,27 +52,29 @@ public class CakeStand {
         finally {
             supplierNumber--;
             supplyCondition.signal();
-            //putCakeCondition.signal();
-            supplyLock.unlock(); // Release the lock
+            putCakeCondition.signal();
+            lock.unlock(); // Release the lock
         }
     }
     public static void putCake(int slices){
         System.out.println(Thread.currentThread().getName() + " baked a cake with "+slices+" slices");
-        putLock.lock(); // Acquire the lock
+        lock.lock(); // Acquire the lock
         try {
             while(bakerNumber > 0){
                 bakerCondition.await();
             }
             bakerNumber++;
-            while (availableStandNumber > 0) {
+            while (availableStandNumber < 1) {
                 putCakeCondition.await();
             }
-            CakeStand selectedStand = availableStandList.get(ThreadLocalRandom.current().nextInt(0, synchronizedList.size()));
+            CakeStand selectedStand = availableStandList.get(ThreadLocalRandom.current().nextInt(0, availableStandList.size()));
             selectedStand.leftCakeSlice = slices;
-            availableStandNumber--;
             availableStandList.remove(selectedStand);
+            availableStandNumber--;
+            selectedStand.monsterLock.lock();
             selectedStand.standCondition.signalAll();
-            System.out.println(Thread.currentThread().getName() + "put the cake with "+slices+" slices on "+selectedStand.standName+".");
+            selectedStand.monsterLock.unlock();
+            System.out.println(Thread.currentThread().getName() + " put the cake with "+slices+" slices on "+selectedStand.standName+".");
         }
         catch (InterruptedException ex) {
             ex.printStackTrace();
@@ -81,12 +82,12 @@ public class CakeStand {
         finally {
             bakerNumber--;
             bakerCondition.signal();
-            putLock.unlock(); // Release the lock
+            lock.unlock(); // Release the lock
         }
     }
     public static CakeStand randomStand(){
         // TODO: Check if synchronizedList is empty.
-        supplyLock.lock();
+        lock.lock();
         try {
             while(standNumber == 0){
                 anyStandCondition.await();
@@ -96,24 +97,31 @@ public class CakeStand {
             ex.printStackTrace();
         }
         finally {
-            supplyLock.unlock(); // Release the lock
+            lock.unlock(); // Release the lock
         }
         return synchronizedList.get(ThreadLocalRandom.current().nextInt(0, synchronizedList.size()));
     }
     public void getSlice(){
         System.out.println(Thread.currentThread().getName() + " came to "+this.standName+" for a slice.");
-        monsterLock.lock(); // Acquire the lock
+        this.monsterLock.lock(); // Acquire the lock
         try {
             while(leftCakeSlice < 1){
-                standCondition.await();
+                this.standCondition.await();
             }
             System.out.println(Thread.currentThread().getName() + " got a slice from "+this.standName+", so "+--leftCakeSlice+" slices left.");
+            if (leftCakeSlice == 0){
+                lock.lock();
+                availableStandList.add(this);
+                availableStandNumber++;
+                putCakeCondition.signal();
+                lock.unlock();
+            }
         }
-        catch (InterruptedException ex) {
+        catch (Exception ex) {
             ex.printStackTrace();
         }
         finally {
-            monsterLock.unlock(); // Release the lock
+            this.monsterLock.unlock(); // Release the lock
         }
     }
 }
